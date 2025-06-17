1afb71d5fe0d525163159b2476cc29c2444deccb
akashspunnayil <akashspunnayil@gmail.com>
2025-06-18 00:06:22 +0530

    updates

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -30,6 +30,7 @@ st.markdown("""
             background-repeat: repeat;
             # background-size: auto;
             background-size: 60px 60px;
+            background-color: rgba(10,10,10,0.9);  /* subtle dark overlay */
             background-color: var(--background-color);
         }
     }
