963c774ef1372b843aff66fdef40026bd1b5987d
akashspunnayil <akashspunnayil@gmail.com>
2025-06-18 00:47:18 +0530

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,37 +8,38 @@ from PIL import Image
 st.set_page_config(page_title="AKASH.S", layout="wide")
 
 
+# st.markdown("""
+#     <style>
+#     [data-testid="stAppViewContainer"] {
+#         background-image: url('https://www.transparenttextures.com/patterns/cubes.png');
+#         background-repeat: repeat;
+#         # background-size: auto;
+#         # background-color: rgba(255,255,255,0.95);
+#         background-size: 60px 60px;
+
+
+#     }
+#     </style>
+# """, unsafe_allow_html=True)
+
 st.markdown("""
     <style>
-    [data-testid="stAppViewContainer"] {
+    html[data-theme="dark"] body {
         background-image: url('https://www.transparenttextures.com/patterns/cubes.png');
         background-repeat: repeat;
-        # background-size: auto;
-        # background-color: rgba(255,255,255,0.95);
         background-size: 60px 60px;
+        background-color: rgba(10, 10, 10, 0.8);
+    }
 
-
+    html[data-theme="light"] body {
+        background-image: url('https://www.transparenttextures.com/patterns/white-wall-3.png');
+        background-repeat: repeat;
+        background-size: 60px 60px;
+        background-color: rgba(255, 255, 255, 0.9);
     }
     </style>
 """, unsafe_allow_html=True)
 
-# st.markdown("""
-#     <style>
-#     html[data-theme="dark"] [data-testid="stAppViewContainer"] > .main {
-#         background-image: url('https://www.transparenttextures.com/patterns/cubes.png') !important;
-#         background-repeat: repeat !important;
-#         background-size: 60px 60px !important;
-#         background-color: rgba(10, 10, 10, 0.8) !important;
-#     }
-
-#     html[data-theme="light"] [data-testid="stAppViewContainer"] > .main {
-#         background-image: url('https://www.transparenttextures.com/patterns/white-wall-3.png') !important;
-#         background-repeat: repeat !important;
-#         background-size: 60px 60px !important;
-#         background-color: rgba(255, 255, 255, 0.9) !important;
-#     }
-#     </style>
-# """, unsafe_allow_html=True)
 
 
 
