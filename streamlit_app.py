21db5e6035d76d15cc43eea5c68dd3611a7fcf50
akashspunnayil <akashspunnayil@gmail.com>
2025-06-25 17:41:38 +0530

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1012,7 +1012,7 @@ elif menu == "🛠️ Projects":
             with col:
                 st.markdown(render_tile(
                     title=title,
-                    url=f"https://github.com/akashspunnayil/AI_ML_DS_projects/tree/master/{link}",
+                    url=f"https://github.com/akashspunnayil/AI_ML_DS_projects/blob/master/{link}",
                     description="Click to view notebook."
                     # In future: img_base64=get_base64_image("static/preview/your_custom.png")
                 ), unsafe_allow_html=True)
