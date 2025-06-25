644744f1e131a2a8ac5f92b273e15da3249f11c5
akashspunnayil <akashspunnayil@gmail.com>
2025-06-25 20:24:29 +0530

    minor updation

--- streamlit_app.py
+++ streamlit_app.py
@@ -726,7 +726,6 @@ elif menu == "🔬 Research":
     ("Experiment Findings", 2),
     ("Presentation", 4),
     ("Poster", 16),
-    ("Full-texts", 5),
     ("", ""),  # Empty row
     ("", ""),  # Another empty row
     ("Total Research Items", 30)
@@ -899,7 +898,7 @@ elif menu == "🛠️ Projects":
     with col1:
         st.markdown(render_tile(
             title="Car Price Prediction Dashboard",
-            url="https://github.com/akashspunnayil/AI_ML_DS_projects/blob/master/1_Automobiles_EDA_ML.ipynb",
+            url="https://github.com/akashspunnayil/AI_ML_DS_projects/tree/master/1_Automobiles_EDA_ML",
             description="EDA and regression modeling on automobile pricing data."
         ), unsafe_allow_html=True)
 
@@ -1012,7 +1011,7 @@ elif menu == "🛠️ Projects":
             with col:
                 st.markdown(render_tile(
                     title=title,
-                    url=f"https://github.com/akashspunnayil/AI_ML_DS_projects/blob/master/{link}",
+                    url=f"https://github.com/akashspunnayil/AI_ML_DS_projects/tree/master/{title}",
                     description="Click to view notebook."
                     # In future: img_base64=get_base64_image("static/preview/your_custom.png")
                 ), unsafe_allow_html=True)
